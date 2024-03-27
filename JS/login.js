671aad26d9ece4e1b6ac72686c25cf9169cf81f4
Igor Llancapan <55776451+illancapan@users.noreply.github.com>
2024-03-27 09:51:00 -0300

    Update login.js espero sirva

--- JS/login.js
+++ JS/login.js
@@ -1,27 +1,57 @@
 
+// let usuarioValido = "usuario123";
+// let contrasenaValida = "claveSecreta";
+
+// function verificarCredenciales(event){
+
+//     const usuarioIngresado = document.getElementById('usuario').value;
+//     const contrasenaIngresada = document.getElementById('contrasena').value;
+
+//     console.log(usuarioIngresado)
+//     console.log(contrasenaIngresada)
+//     if (usuarioIngresado === usuarioValido && contrasenaIngresada === contrasenaValida) {
+//         alert('Inicio de sesión exitoso');
+//         window.location.href = "../menu.html";
+// console.log("ingreso realizado primera prueba")
+        
+//     } 
+    
+//     else {
+        
+//         alert('Credenciales incorrectas');
+
+// console.log("ingreso no realizado")
+
+//     }
+// console.log("ingreso realizado primera prueba")
+
+// } 
+
+// document.getElementById("login-form").addEventListener("submit", verificarCredenciales);
+
 let usuarioValido = "usuario123";
 let contrasenaValida = "claveSecreta";
 
-function verificarCredenciales(event){
+function verificarCredenciales(event) {
+    event.preventDefault(); // formulario se envie
 
     const usuarioIngresado = document.getElementById('usuario').value;
     const contrasenaIngresada = document.getElementById('contrasena').value;
 
+    console.log("Usuario ingresado:", usuarioIngresado);
+    console.log("Contraseña ingresada:", contrasenaIngresada);
+
     if (usuarioIngresado === usuarioValido && contrasenaIngresada === contrasenaValida) {
+        console.log("Inicio de sesión exitoso");
         alert('Inicio de sesión exitoso');
         window.location.href = "../menu.html";
-
-        
-    } 
-    
-    else {
-        
+    } else {
+        console.log("Credenciales incorrectas");
         alert('Credenciales incorrectas');
     }
-} 
+}
 
 document.getElementById("login-form").addEventListener("submit", verificarCredenciales);
-
 
 
 
